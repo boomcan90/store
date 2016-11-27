8501d8656d4d881171c6057a33801f964d6dac17
Robin Lee <robin.lee.jw@gmail.com>
2016-11-28 00:03:56 +0800

    temporary user login template. some user functional tests

--- store/user/views.py
+++ store/user/views.py
@@ -1,16 +1,52 @@
 # -*- coding: utf-8 -*-
 """User views."""
 
-from flask import Blueprint, render_template
+from flask import Blueprint, flash, redirect, render_template, request, url_for
 
-from flask_login import login_required
+from flask_login import login_required, login_user
+
+from store.public.forms import LoginForm
+
+from store.user.forms import RegisterForm
+
+from store.utils import flash_errors
+
+from .models import User
 
 
-blueprint = Blueprint('user', __name__, url_prefix='/users', static_folder='../static')
+blueprint = Blueprint('user', __name__, url_prefix='/user', static_folder='../static')
 
 
 @blueprint.route('/')
 @login_required
 def members():
     """List members."""
-    return render_template('users/members.html')
+    return render_template('user/members.html')
+
+
+@blueprint.route('/login/', methods=['GET', 'POST'])
+def login():
+    form = LoginForm(request.form)
+    # Handle logging in
+    if request.method == 'POST':
+        if form.validate_on_submit():
+            login_user(form.user)
+            flash('You are logged in.', 'success')
+            redirect_url = request.args.get('next') or url_for('user.members')
+            return redirect(redirect_url)
+        else:
+            flash_errors(form)
+    return render_template('user/login.html', form=form)
+
+
+@blueprint.route('/register/', methods=['GET', 'POST'])
+def register():
+    """Register new user."""
+    form = RegisterForm(request.form, csrf_enabled=False)
+    if form.validate_on_submit():
+        User.create(id=form.username.data, email=form.email.data, password=form.password.data, active=True)
+        flash('Thank you for registering. You can now log in.', 'success')
+        return redirect(url_for('public.home'))
+    else:
+        flash_errors(form)
+    return render_template('user/register.html', form=form)
--- tests/test_user_functional.py
+++ tests/test_user_functional.py
@@ -1,29 +1,29 @@
-# # -*- coding: utf-8 -*-
-# """Functional tests using WebTest.
+# -*- coding: utf-8 -*-
+"""Functional tests using WebTest.
 
-# See: http://webtest.readthedocs.org/
-# """
-# from flask import url_for
+See: http://webtest.readthedocs.org/
+"""
+from flask import url_for
 
-# from store.user.models import User
+from store.user.models import User
 
-# from .factories import UserFactory
+from .factories import UserFactory
 
 
-# class TestLoggingIn:
-#     """Login."""
+class TestLoggingIn:
+    """Login."""
 
-#     def test_can_log_in_returns_200(self, user, testapp):
-#         """Login successful."""
-#         # Goes to homepage
-#         res = testapp.get('/')
-#         # Fills out login form in navbar
-#         form = res.forms['loginForm']
-#         form['username'] = user.id
-#         form['password'] = 'myprecious'
-#         # Submits
-#         res = form.submit().follow()
-#         assert res.status_code == 200
+    def test_can_log_in_returns_200(self, user, testapp):
+        """Login successful."""
+        # Goes to homepage
+        res = testapp.get('/user/login/')
+        # Fills out login form in navbar
+        form = res.forms['loginForm']
+        form['username'] = user.id
+        form['password'] = 'myprecious'
+        # Submits
+        res = form.submit().follow()
+        assert res.status_code == 200
 
 #     def test_sees_alert_on_log_out(self, user, testapp):
 #         """Show alert on logout."""
@@ -38,18 +38,18 @@
 #         # sees alert
 #         assert 'You are logged out.' in res
 
-#     def test_sees_error_message_if_password_is_incorrect(self, user, testapp):
-#         """Show error if password is incorrect."""
-#         # Goes to homepage
-#         res = testapp.get('/')
-#         # Fills out login form, password incorrect
-#         form = res.forms['loginForm']
-#         form['username'] = user.id
-#         form['password'] = 'wrong'
-#         # Submits
-#         res = form.submit()
-#         # sees error
-#         assert 'Invalid password' in res
+    def test_sees_error_message_if_password_is_incorrect(self, user, testapp):
+        """Show error if password is incorrect."""
+        # Goes to homepage
+        res = testapp.get('/user/login/')
+        # Fills out login form, password incorrect
+        form = res.forms['loginForm']
+        form['username'] = user.id
+        form['password'] = 'wrong'
+        # Submits
+        res = form.submit()
+        # sees error
+        assert 'Invalid password' in res
 
 #     def test_sees_error_message_if_username_doesnt_exist(self, user, testapp):
 #         """Show error if username doesn't exist."""
