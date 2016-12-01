8823036171d60bbcb6275e9a710c02e00ba5ed95
Robin Lee <robin.lee.jw@gmail.com>
2016-12-01 20:12:33 +0800

    Update book adding feature.

--- store/book/forms.py
+++ store/book/forms.py
@@ -1,31 +1,40 @@
 # -*- coding: utf-8 -*-
 """Book forms."""
 from flask_wtf import Form
-from wtforms import StringField,IntegerField
-from wtforms.validators import DataRequired, EqualTo, Length
+from wtforms import StringField, IntegerField
+from wtforms.validators import DataRequired, Length
 
 from store.book.models import Book
 
-class AddBookForm(Form):
-    """Add Book Form"""
 
-    isbn13 = StringField('isbn13',validators=[DataRequired(), Length(min=1, max=13)])
-    title =  StringField('title',validators=[DataRequired(), Length(min=3, max=128)])
-    author = StringField('author',validators=[DataRequired(), Length(min=3, max=128)])
-    publisher = StringField('publisher',validators=[DataRequired(), Length(min=3, max=128)])
-    year_of_pub = IntegerField('year_of_pub',validators=[DataRequired()])
-    num_of_copies = IntegerField('num_of_copies',validators=[DataRequired()])
-    price = IntegerField('price',validators=[DataRequired()])
-    format = StringField('format',validators=[DataRequired(), Length(min=3, max=128)])
-    keywords = StringField('keywords',validators=[DataRequired(), Length(min=3, max=128)])
-    subject = StringField('subject',validators=[DataRequired(), Length(min=3, max=128)])
+class AddBookForm(Form):
+    """Add Book Form."""
+
+    isbn13 = StringField('isbn13', validators=[
+                         DataRequired(), Length(min=13, max=13)])
+    title = StringField('title', validators=[
+                        DataRequired(), Length(min=3, max=128)])
+    author = StringField('author', validators=[
+                         DataRequired(), Length(min=3, max=128)])
+    publisher = StringField('publisher', validators=[
+                            DataRequired(), Length(min=3, max=128)])
+    year_of_pub = IntegerField('year_of_pub', validators=[DataRequired()])
+    num_of_copies = IntegerField('num_of_copies', validators=[DataRequired()])
+    price = IntegerField('price', validators=[DataRequired()])
+    format = StringField('format', validators=[
+                         DataRequired(), Length(min=3, max=128)])
+    keywords = StringField('keywords', validators=[
+                           DataRequired(), Length(min=3, max=128)])
+    subject = StringField('subject', validators=[
+                          DataRequired(), Length(min=3, max=128)])
 
     def __init__(self, *args, **kwargs):
+        """Init."""
         super(AddBookForm, self).__init__(*args, **kwargs)
         self.book = None
 
     def validate(self):
-        """validate add book form"""
+        """Validate add book form."""
         initial_validation = super(AddBookForm, self).validate()
         if not initial_validation:
             return False
--- store/book/views.py
+++ store/book/views.py
@@ -1,34 +1,50 @@
 # -*- coding: utf-8 -*-
 """Book views."""
-from flask import Blueprint, render_template, jsonify
+from flask import Blueprint, flash, redirect, render_template, request, url_for
 from store.book.models import Book
+from store.book.forms import AddBookForm
+
+from store.utils import flash_errors
 
 
-book_blueprint = Blueprint('book', __name__, url_prefix='/book', static_folder='../static')
+book_blueprint = Blueprint(
+    'book', __name__, url_prefix='/book', static_folder='../static')
 
-#127.0.0.1:5000/book/
+
 @book_blueprint.route('/')
 def hello():
-    '''
-    A trivial route.
-    '''
+    """A trivial route."""
     return "hello!"
 
-#127.0.0.1:5000/book/book
+
 @book_blueprint.route('/book', methods=['GET'])
 def book():
-    """List 1 book"""
+    """List 1 book."""
     book = Book.query.first()
     # return "Book details"
     return render_template('book/book.html', book=book)
 
-#127.0.0.1:5000/book/books
-@book_blueprint.route('/books', methods=['GET'])
-def bookList():
 
-    # recipe = session.query(Recipe, Ingredient).filter(Recipe.id==Ingredient.recipe_id).filter(Ingredient.name.contains(query)).all()
+@book_blueprint.route('/books', methods=['GET'])
+def book_list():
+    """Return list of books."""
     book = Book.query.all()
 
-    # return jsonify(data = [x.to_json() for x in result])
     return render_template('book/booklist.html', books=book)
-    
+
+
+@book_blueprint.route('/add', methods=['GET', 'POST'])
+def add_book():
+    """Add book."""
+    form = AddBookForm(request.form)
+    # Handle add book form
+    if request.method == 'POST':
+        if form.validate_on_submit():
+            Book.create(book)
+            flash('You are added the book!', 'success')
+            redirect_url = request.args.get(
+                'next') or url_for('book.add_book')
+            return redirect(redirect_url)
+        else:
+            flash_errors(form)
+    return render_template('book/addbooks.html', form=form)
