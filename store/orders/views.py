66d42e8b24742d7721a86d9b77f01d596acaf046
Robin Lee <robin.lee.jw@gmail.com>
2016-12-10 18:23:04 +0800

    Merge add orders, recommendation, stats from Dhanya.

--- store/orders/views.py
+++ store/orders/views.py
@@ -4,7 +4,7 @@
 from flask import Blueprint, flash, redirect, render_template, request, url_for, abort, jsonify
 from flask_login import current_user, login_required  # noqa
 
-from store.orders.models import Order, OrderConsistsOf
+from store.orders.models import Order, OrderConsistsOf, Book
 from store.orders.forms import AddOrderForm
 
 from store.utils import flash_errors
@@ -44,8 +44,54 @@ def browse_list():
     result = Order.query.all()
     return jsonify(data=[x.to_json() for x in result])
 
-@order_blueprint.route('/orderBook/<customerid>/<isbn13>', methods=['GET', 'POST'])
-def order_book(isbn13, customerid):
+@order_blueprint.route('/popular_books', methods=['GET'])
+def pop_books():
+    popular_books = []
+    q1 = OrderConsistsOf.query.all()
+    q2 = db.session.query(OrderConsistsOf.consists_isbn13, func.count(OrderConsistsOf.consists_qty))\
+                    .group_by(OrderConsistsOf.consists_isbn13).all()
+    for p2 in q2:
+        count = 0
+        for p1 in q1:
+            if(p1.to_json()['isbn13'] == p2[0]):
+                count += p1.to_json()['quantity']
+        popular_books.append((p2[0], count))
+    return render_template("user/popular_books.html", popular_books=popular_books)
+# ----------------------------------------------------------------
+@order_blueprint.route('/popular_authors', methods=['GET'])
+def pop_authors():
+    popular_authors = []
+    q = db.session.query(OrderConsistsOf.consists_isbn13, Book.author, OrderConsistsOf.consists_qty).\
+         join(Book).filter(Book.isbn13 == OrderConsistsOf.consists_isbn13).all()
+    authors = list(set([p[1] for p in q]))
+    for p1 in authors:
+        count = 0
+        for p2 in q:
+            if p1 == p2[1]:
+                count += p2[2]
+        popular_authors.append((p1, count))
+    return render_template("user/popular_authors.html", popular_authors=popular_authors)
+# ----------------------------------------------------------------
+@order_blueprint.route('/popular_publishers', methods=['GET'])
+def pop_publishers():
+    popular_publishers = []
+    q = db.session.query(OrderConsistsOf.consists_isbn13, Book.publisher, OrderConsistsOf.consists_qty).\
+                         join(Book).filter(Book.isbn13 == OrderConsistsOf.consists_isbn13).all()
+    publishers = list(set([p[1] for p in q]))
+    # print(publishers)
+    for p1 in publishers:
+        count = 0
+        for p2 in q:
+            if p1 == p2[1]:
+                count += p2[2]
+        popular_publishers.append((p1, count))
+    return render_template("user/popular_publishers.html", popular_publishers=popular_publishers)
+# ----------------------------------------------------------------
+
+
+@order_blueprint.route('/orderBook/<isbn13>', methods=['GET', 'POST'])
+def order_book(isbn13):
+    customerid = current_user.get_id()
     form = AddOrderForm(request.form)
     if request.method == 'POST':
         order = Order.create(customer_id=customerid)
@@ -57,7 +103,7 @@ def order_book(isbn13, customerid):
                                consists_isbn13=isbn13, consists_qty=form.qty.data)
         flash('You have ordered the book: {}!'.format(
                 isbn13), 'success')
-        return "Good job!"
+        return redirect(url_for('book.browse'))
     else:
         flash_errors(form)
     return render_template('orders/addorder.html', form=form)
@@ -66,22 +112,30 @@ def order_book(isbn13, customerid):
 @order_blueprint.route('/orderHistory/<id>', methods=['GET'])
 def order_history(id):
     """Order history."""
-    q = OrderConsistsOf.query.join(Order, (OrderConsistsOf.consists_order_id == Order.id)).\
-            filter(Order.customer_id == id).all()
+    q = OrderConsistsOf.query.join(Order, (OrderConsistsOf.consists_order_id == Order.id)).filter(Order.customer_id == id).all()
 
     return jsonify(data=[x.to_json() for x in q])
 
-# @order_blueprint.route('/recommend/<isbn13>', methods=['GET'])
-# def reccomendation(isbn13):
-#     """Get book recommendation"""
-#     if current_user.is_authenticated:
-#         id = current_user.get_id()
-#         # get the book rec
-#         q1 = OrderConsistsOf.query.filter(OrderConsistsOf.consists_isbn13).all()
+@order_blueprint.route('/recommendation/<isbn13>', methods=['GET'])
+def reccomendation(isbn13):
+    RECCOMENDATIONS = []
+    """Get book recommendation"""
+    if current_user.is_authenticated:
+        id = current_user.get_id()
+        # get the book rec
+        q1 = db.session.query(OrderConsistsOf.consists_order_id).filter(OrderConsistsOf.consists_isbn13 == isbn13).\
+                              filter(OrderConsistsOf.consists_order_id != id).all()
+        
+        for q in q1:
+            isbn13_list = db.session.query(OrderConsistsOf.consists_isbn13).filter(OrderConsistsOf.consists_order_id == q[0]).\
+                                     filter(OrderConsistsOf.consists_isbn13 != isbn13).all()    
+            if isbn13_list != []:
+                for i in isbn13_list:
+                    RECCOMENDATIONS.append(db.session.query(Book).filter(Book.isbn13 == i[0]).first())
 
-#         list_of_orderid = [x.to_json() for x in result]
+    # return jsonify(data=[x.to_json() for x in RECCOMENDATIONS])
+    return render_template('orders/recommendation.html', recommendations = RECCOMENDATIONS)
 
-#         for element in list
 
 # ---------------------------------------------------------------------------
 
